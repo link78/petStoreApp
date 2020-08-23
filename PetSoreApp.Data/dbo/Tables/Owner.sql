2f8e37f372c948af2431d555ad1c3c25ea4d6525
Kade <kaderderk@gmail.com>
2020-08-23 16:57:30 -0500

    added database project

---
+++ PetSoreApp.Data/dbo/Tables/Owner.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Owner] (
+    [OwnerId]     INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NVARCHAR (MAX) NULL,
+    [LastName]    NVARCHAR (MAX) NULL,
+    [email]       NVARCHAR (MAX) NULL,
+    [Photo]       NVARCHAR (MAX) NULL,
+    [PhoneNumber] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Owner] PRIMARY KEY CLUSTERED ([OwnerId] ASC)
+);
+
